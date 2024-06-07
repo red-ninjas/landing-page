35782ad8b0e5c599f9ea251e8a43d9a4d6031120
Syed Abubakar <developer@awbnext.com>
2024-06-07 12:44:23 +0500

    fix: reviews

--- src/lib/rest/get-reviews.ts
+++ src/lib/rest/get-reviews.ts
@@ -4,8 +4,10 @@ export const getReviews = cache(async (): Promise<any[]> => {
   const url = `https://maps.googleapis.com/maps/api/place/details/json?key=${process.env.GOOGLE_KEY}&place_id=${process.env.GOOGLE_PLACE_ID}`;
   const res = await fetch(url);
   const resJson = await res.json();
-  console.log(resJson)
   
-  // const reviews = resJson.result.reviews.filter(review => review.rating === 5);
+  if (resJson.result && resJson.result.reviews) {
+    return resJson.result.reviews as any[];
+  }
+  
   return [] as any[];
 });
